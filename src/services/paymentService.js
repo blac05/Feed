9458096fafda05f6c82e1225d59d7f9335ee78f7
Joe Jae <blaqj23@gmail.com>
2026-06-03 06:51:38 +0100

    Add coinPackages array for payment options

---
+++ src/services/paymentService.js
@@ -0,0 +1,25 @@
+export const coinPackages = [
+  {
+    id: 1,
+    coins: 100,
+    price: 1,
+  },
+
+  {
+    id: 2,
+    coins: 500,
+    price: 5,
+  },
+
+  {
+    id: 3,
+    coins: 1000,
+    price: 10,
+  },
+
+  {
+    id: 4,
+    coins: 5000,
+    price: 50,
+  },
+];
