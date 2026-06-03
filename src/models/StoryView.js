ec38fcf9c25a0365f4838a2c63c90475e4845222
Joe Jae <blaqj23@gmail.com>
2026-06-03 12:29:22 +0100

    Add StoryView model with viewer and story references

---
+++ src/models/StoryView.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const storyViewSchema =
+  new mongoose.Schema(
+    {
+      story: {
+        type:
+          mongoose.Schema.Types.ObjectId,
+        ref: "Story",
+      },
+
+      viewer: {
+        type:
+          mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+export default mongoose.model(
+  "StoryView",
+  storyViewSchema
+);
