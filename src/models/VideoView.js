8ac0fb58c691568e290ec2a5e674afed20f52f28
Joe Jae <blaqj23@gmail.com>
2026-06-04 02:09:47 +0100

    Create VideoView model with schema and index

--- src/models/VideoView.js
+++ src/models/VideoView.js
@@ -1,1 +1,38 @@
+import mongoose from "mongoose";
 
+const videoViewSchema =
+  new mongoose.Schema(
+    {
+      video: {
+        type:
+          mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+        required: true,
+      },
+
+      user: {
+        type:
+          mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+videoViewSchema.index(
+  {
+    video: 1,
+    user: 1,
+  },
+  {
+    unique: true,
+  }
+);
+
+export default mongoose.model(
+  "VideoView",
+  videoViewSchema
+);
