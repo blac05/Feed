be5b61d32f74c0fa75555b891316fb51e86adc07
Joe Jae <blaqj23@gmail.com>
2026-06-05 02:30:30 +0100

    Add admin authentication middleware

---
+++ src/middleware/adminAuth.js
@@ -0,0 +1,19 @@
+export default (
+  req,
+  res,
+  next
+)=>{
+  if(
+    req.user.role !==
+    "admin"
+  ){
+    return res
+      .status(403)
+      .json({
+        success:false,
+        message:"Admin only"
+      });
+  }
+
+  next();
+};
