5ae452d2f6a561610558a6018a094a319d34f54f
Joe Jae <blaqj23@gmail.com>
2026-06-05 02:26:03 +0100

    Add Report model with schema for reporting entities

--- src/models/Report.js
+++ src/models/Report.js
@@ -1,1 +1,39 @@
+import mongoose from "mongoose";
 
+const reportSchema =
+new mongoose.Schema({
+
+  reporter:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User"
+  },
+
+  targetId:String,
+
+  targetType:{
+    type:String,
+    enum:[
+      "post",
+      "video",
+      "story",
+      "comment",
+      "live",
+      "user"
+    ]
+  },
+
+  reason:String,
+
+  status:{
+    type:String,
+    default:"pending"
+  }
+},
+{
+  timestamps:true
+});
+
+export default mongoose.model(
+  "Report",
+  reportSchema
+);
