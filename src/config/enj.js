ca1cb36cdc1e29286d7cebc4ef3587bf5399150c
Joe Jae <blaqj23@gmail.com>
2026-06-08 04:09:50 +0100

    Update env.js with default PORT and MONGODB_URI
    Set default PORT and update MONGODB_URI with credentials.

--- src/config/enj.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const env = {
-  PORT: process.env.PORT,
-  JWT_SECRET: process.env.JWT_SECRET,
-  MONGODB_URI: process.env.MONGODB_URI,
-};
---
+++ src/config/env.js
@@ -0,0 +1,8 @@
+export const env = {
+  PORT: process.env.PORT || 5000,
+  MONGODB_URI: mongodb+srv://Gibson:<Hatrick77>@cluster0.udpjlbu.mongodb.net/?appName=Cluster0,
+  JWT_SECRET: process.env.JWT_SECRET,
+  CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+  CLOUDINARY_KEY: process.env.CLOUDINARY_KEY,
+  CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET,
+};
