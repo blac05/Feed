3708135bd97dfdaea5358b6cda0d266147c663d9
Joe Jae <blaqj23@gmail.com>
2026-06-18 01:27:53 +0100

    Modify registerUser to include name and accountType

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,24 +1,18 @@
 import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 
-export const registerUser = async (
-  username,
-  email,
-  password
-) => {
+export const registerUser = async (username, name, email, password, accountType = "personal") => {
   const exists = await User.findOne({ email });
+  if (exists) throw new Error("User already exists");
 
-  if (exists) {
-    throw new Error("User already exists");
-  }
-
-  const hashedPassword =
-    await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = await User.create({
     username,
+    name,
     email,
     password: hashedPassword,
+    accountType,
   });
 
   return user;
