3ebb0fd790fafaf4d6a95218b6a4e78c2f398b49
Joe Jae <blaqj23@gmail.com>
2026-06-18 01:26:34 +0100

    Refactor Post schema to simplify structure

--- src/models/Post.js
+++ src/models/Post.js
@@ -2,29 +2,23 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema(
   {
-    author: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-
-    content: {
-      type: String,
-      required: true,
-    },
-
-    image: String,
-
-    likes: [
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true },
+    image: { type: String, default: "" },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comments: [
       {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        text: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now },
       },
     ],
+    reposts: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    tags: [{ type: String }],
+    type: { type: String, enum: ["post", "news", "update", "headline"], default: "post" },
+    views: { type: Number, default: 0 },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
 export default mongoose.model("Post", postSchema);
