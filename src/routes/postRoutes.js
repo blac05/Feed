829ca118b4e8ea8e98959df8aaf79d7626a76d14
Joe Jae <blaqj23@gmail.com>
2026-06-18 01:41:52 +0100

    Update postRoutes.js

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,33 +1,21 @@
 import express from "express";
-
 import auth from "../middleware/auth.js";
-
 import {
   createPost,
   getPosts,
   getPost,
   deletePost,
   likePost,
+  addComment,
 } from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
-
 router.get("/:id", getPost);
-
 router.post("/", auth, createPost);
-
-router.delete(
-  "/:id",
-  auth,
-  deletePost
-);
-
-router.put(
-  "/:id/like",
-  auth,
-  likePost
-);
+router.delete("/:id", auth, deletePost);
+router.put("/:id/like", auth, likePost);
+router.post("/:id/comment", auth, addComment);
 
 export default router;
