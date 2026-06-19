8184037968ebfb14c520930c61ca3fa6cad35a81
Joe Jae <blaqj23@gmail.com>
2026-06-19 13:05:54 +0100

    Add getFollowingPosts function and refactor error handling

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,6 +1,7 @@
 import {
   createPostService,
   getAllPostsService,
+  getFollowingPostsService,
   getPostByIdService,
   deletePostService,
   likePostService,
@@ -11,52 +12,47 @@ export const createPost = async (req, res, next) => {
   try {
     const post = await createPostService(req.user._id, req.body);
     res.status(201).json({ success: true, post });
-  } catch (error) {
-    next(error);
-  }
+  } catch (error) { next(error); }
 };
 
 export const getPosts = async (req, res, next) => {
   try {
     const posts = await getAllPostsService();
     res.json({ success: true, posts });
-  } catch (error) {
-    next(error);
-  }
+  } catch (error) { next(error); }
+};
+
+export const getFollowingPosts = async (req, res, next) => {
+  try {
+    const posts = await getFollowingPostsService(req.user._id);
+    res.json({ success: true, posts });
+  } catch (error) { next(error); }
 };
 
 export const getPost = async (req, res, next) => {
   try {
     const post = await getPostByIdService(req.params.id);
     res.json({ success: true, post });
-  } catch (error) {
-    next(error);
-  }
+  } catch (error) { next(error); }
 };
 
 export const deletePost = async (req, res, next) => {
   try {
     await deletePostService(req.params.id, req.user._id.toString());
     res.json({ success: true, message: "Post deleted" });
-  } catch (error) {
-    next(error);
-  }
+  } catch (error) { next(error); }
 };
 
 export const likePost = async (req, res, next) => {
   try {
     const post = await likePostService(req.params.id, req.user._id);
     res.json({ success: true, post });
-  } catch (error) {
-    next(error);
-  }
+  } catch (error) { next(error); }
 };
 
 export const addComment = async (req, res, next) => {
   try {
     const post = await addCommentService(req.params.id, req.user._id, req.body.text);
     res.json({ success: true, post });
-  } catch (error) {
-    next(error);
-  }
+  } catch (error) { next(error); }
 };
