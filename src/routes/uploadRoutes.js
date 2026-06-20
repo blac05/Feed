b4eeb518715cfda0c17bb0ecf4cc4d2c225c54bc
Joe Jae <blaqj23@gmail.com>
2026-06-20 11:30:13 +0000

    Update uploadRoutes.js

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import upload from "../middleware/upload.js";
 import auth from "../middleware/auth.js";
-import { uploadImage, uploadAvatar } from "../controllers/uploadController.js";
+import { uploadImage, uploadAvatar, uploadVideo } from "../controllers/uploadController.js";
 
 const router = express.Router();
 
 router.post("/image", auth, upload.single("image"), uploadImage);
 router.post("/avatar", auth, upload.single("avatar"), uploadAvatar);
+router.post("/video", auth, upload.single("video"), uploadVideo);
 
 export default router;
+
