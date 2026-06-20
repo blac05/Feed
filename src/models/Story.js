876365adc5e497e5f30b6a122acc238ae9557adf
Joe Jae <blaqj23@gmail.com>
2026-06-20 11:22:55 +0000

    Update Story.js

--- src/models/Story.js
+++ src/models/Story.js
@@ -1,33 +1,19 @@
 import mongoose from "mongoose";
 
-const storySchema =
-  new mongoose.Schema(
-    {
-      user: {
-        type:
-          mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-
-      media: String,
-
-      caption: String,
-
-      expiresAt: {
-        type: Date,
-        default: () =>
-          new Date(
-            Date.now() +
-              24 * 60 * 60 * 1000
-          ),
-      },
+const storySchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    image: { type: String, required: true },
+    caption: { type: String, default: "" },
+    views: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    expiresAt: {
+      type: Date,
+      default: () => new Date(Date.now() + 24 * 60 * 60 * 1000),
     },
-    {
-      timestamps: true,
-    }
-  );
-
-export default mongoose.model(
-  "Story",
-  storySchema
+  },
+  { timestamps: true }
 );
+
+export default mongoose.model("Story", storySchema);
+
