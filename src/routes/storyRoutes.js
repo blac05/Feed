be2fffbbe1bd73bd0401b964a9b89af62acc3506
Joe Jae <blaqj23@gmail.com>
2026-06-20 11:24:19 +0000

    Update storyRoutes.js

--- src/routes/storyRoutes.js
+++ src/routes/storyRoutes.js
@@ -1,25 +1,17 @@
 import express from "express";
-
 import auth from "../middleware/auth.js";
-
 import {
-  createStory,
-  getStories,
+  createStory, getStories, viewStory,
+  likeStory, deleteStory,
 } from "../controllers/storyController.js";
 
-const router =
-  express.Router();
+const router = express.Router();
 
-router.post(
-  "/",
-  auth,
-  createStory
-);
-
-router.get(
-  "/",
-  auth,
-  getStories
-);
+router.get("/", auth, getStories);
+router.post("/", auth, createStory);
+router.put("/:id/view", auth, viewStory);
+router.put("/:id/like", auth, likeStory);
+router.delete("/:id", auth, deleteStory);
 
 export default router;
+
