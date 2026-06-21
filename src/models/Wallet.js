0ab5c6a67a3517eb8e9e294332d623149dc1802a
Joe Jae <blaqj23@gmail.com>
2026-06-21 04:01:31 +0000

    Update Wallet.js

--- src/models/Wallet.js
+++ src/models/Wallet.js
@@ -1,12 +1,19 @@
 import mongoose from "mongoose";
 
 const WalletSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  balance: { type: Number, default: 0 },
-  frozenFunds: { type: Number, default: 0 } // Funds currently locked in escrow
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    default: 0,
+  },
+  frozenFunds: {
+    type: Number,
+    default: 0, // Funds currently locked in escrow
+  },
 });
 
-export default mongoose.model(
-  "Wallet",
-  walletSchema
-);
+export default mongoose.model("Wallet", WalletSchema);
