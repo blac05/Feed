a27476a137d993432b3c09bbbedb63a0d89fe7e3
Joe Jae <blaqj23@gmail.com>
2026-06-21 03:20:26 +0000

    Update communityController.js

--- src/controllers/communityController.js
+++ src/controllers/communityController.js
@@ -1,139 +1,63 @@
 import Community from "../models/Community.js";
-import CommunityPost from "../models/CommunityPost.js";
-import CommunityMember from "../models/CommunityMember.js";
 
-export const createCommunity =
-async (req,res,next)=>{
-  try{
-
-    const community =
-      await Community.create({
-
-        ...req.body,
-
-        owner:req.user._id
-      });
-
-    await CommunityMember.create({
-
-      community:community._id,
-
-      user:req.user._id,
-
-      role:"owner"
-    });
-
-    res.status(201).json({
-      success:true,
-      community
-    });
-
-  }catch(error){
-    next(error);
-  }
+export const getCommunities = async (req, res, next) => {
+  try {
+    const communities = await Community.find()
+      .populate("creator", "username name avatar")
+      .sort({ createdAt: -1 });
+    res.json({ success: true, communities });
+  } catch (error) { next(error); }
 };
 
-export const getCommunities =
-async (req,res,next)=>{
-  try{
-
-    const communities =
-      await Community.find()
-      .populate(
-        "owner",
-        "username avatar"
-      );
-
-    res.json({
-      success:true,
-      communities
-    });
-
-  }catch(error){
-    next(error);
-  }
+export const getCommunity = async (req, res, next) => {
+  try {
+    const community = await Community.findById(req.params.id)
+      .populate("creator", "username name avatar isVerified accountType")
+      .populate("members", "username name avatar isVerified accountType");
+    if (!community) return res.status(404).json({ message: "Community not found" });
+    res.json({ success: true, community });
+  } catch (error) { next(error); }
 };
 
-export const joinCommunity =
-async (req,res,next)=>{
-  try{
-
-    await CommunityMember.create({
-
-      community:req.params.id,
-
-      user:req.user._id
+export const createCommunity = async (req, res, next) => {
+  try {
+    const { name, description, category, image, isPrivate, rules } = req.body;
+    const community = await Community.create({
+      name, description, category, image, isPrivate,
+      rules: rules || [],
+      creator: req.user._id,
+      members: [req.user._id],
+      moderators: [req.user._id],
     });
-
-    await Community.findByIdAndUpdate(
-      req.params.id,
-      {
-        $inc:{
-          membersCount:1
-        }
-      }
-    );
-
-    res.json({
-      success:true,
-      message:"Joined community"
-    });
-
-  }catch(error){
-    next(error);
-  }
+    const populated = await Community.findById(community._id)
+      .populate("creator", "username name avatar");
+    res.status(201).json({ success: true, community: populated });
+  } catch (error) { next(error); }
 };
 
-export const createCommunityPost =
-async (req,res,next)=>{
-  try{
-
-    const post =
-      await CommunityPost.create({
-
-        community:req.params.id,
-
-        author:req.user._id,
-
-        content:req.body.content,
-
-        images:req.body.images
-      });
-
-    res.status(201).json({
-      success:true,
-      post
-    });
-
-  }catch(error){
-    next(error);
-  }
+export const joinCommunity = async (req, res, next) => {
+  try {
+    const community = await Community.findById(req.params.id);
+    if (!community) return res.status(404).json({ message: "Community not found" });
+    const isMember = community.members.includes(req.user._id);
+    if (isMember) {
+      community.members.pull(req.user._id);
+    } else {
+      community.members.push(req.user._id);
+    }
+    await community.save();
+    res.json({ success: true, joined: !isMember, memberCount: community.members.length });
+  } catch (error) { next(error); }
 };
 
-export const getCommunityPosts =
-async (req,res,next)=>{
-  try{
-
-    const posts =
-      await CommunityPost.find({
-
-        community:req.params.id
-
-      })
-      .populate(
-        "author",
-        "username avatar"
-      )
-      .sort({
-        createdAt:-1
-      });
-
-    res.json({
-      success:true,
-      posts
-    });
-
-  }catch(error){
-    next(error);
-  }
+export const deleteCommunity = async (req, res, next) => {
+  try {
+    const community = await Community.findById(req.params.id);
+    if (!community) return res.status(404).json({ message: "Not found" });
+    if (community.creator.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
+    await Community.findByIdAndDelete(req.params.id);
+    res.json({ success: true });
+  } catch (error) { next(error); }
 };
