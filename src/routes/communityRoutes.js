5cf569ef73cf578e46fed867fc902df2ce064d34
Joe Jae <blaqj23@gmail.com>
2026-06-21 03:21:07 +0000

    Update communityRoutes.js

--- src/routes/communityRoutes.js
+++ src/routes/communityRoutes.js
@@ -1,49 +1,16 @@
 import express from "express";
-
 import auth from "../middleware/auth.js";
-
 import {
+  getCommunities, getCommunity, createCommunity,
+  joinCommunity, deleteCommunity,
+} from "../controllers/communityController.js";
 
-  createCommunity,
-  getCommunities,
+const router = express.Router();
 
-  joinCommunity,
-
-  createCommunityPost,
-  getCommunityPosts
-
-}
-from "../controllers/communityController.js";
-
-const router =
-express.Router();
-
-router.post(
-  "/",
-  auth,
-  createCommunity
-);
-
-router.get(
-  "/",
-  getCommunities
-);
-
-router.post(
-  "/:id/join",
-  auth,
-  joinCommunity
-);
-
-router.post(
-  "/:id/posts",
-  auth,
-  createCommunityPost
-);
-
-router.get(
-  "/:id/posts",
-  getCommunityPosts
-);
+router.get("/", getCommunities);
+router.get("/:id", getCommunity);
+router.post("/", auth, createCommunity);
+router.post("/:id/join", auth, joinCommunity);
+router.delete("/:id", auth, deleteCommunity);
 
 export default router;
