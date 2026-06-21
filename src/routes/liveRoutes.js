9baab00d6efd0f8278da1f6f3e64d7281e812204
Joe Jae <blaqj23@gmail.com>
2026-06-21 03:18:22 +0000

    Update liveRoutes.js

--- src/routes/liveRoutes.js
+++ src/routes/liveRoutes.js
@@ -1,38 +1,16 @@
 import express from "express";
-
 import auth from "../middleware/auth.js";
-
 import {
-  startLive,
-  joinLive,
-  getLives,
-  endLive,
+  startLive, joinLive, getLives, getLive, endLive, leaveStream
 } from "../controllers/liveController.js";
 
-const router =
-  express.Router();
+const router = express.Router();
 
-router.get(
-  "/",
-  getLives
-);
-
-router.post(
-  "/start",
-  auth,
-  startLive
-);
-
-router.get(
-  "/join/:id",
-  auth,
-  joinLive
-);
-
-router.put(
-  "/end/:id",
-  auth,
-  endLive
-);
+router.get("/", getLives);
+router.get("/:id", auth, getLive);
+router.post("/start", auth, startLive);
+router.get("/join/:id", auth, joinLive);
+router.put("/end/:id", auth, endLive);
+router.put("/leave/:id", auth, leaveStream);
 
 export default router;
