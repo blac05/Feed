a02c087e52da9bffbc583c70632eea8f0ffa71ae
Joe Jae <blaqj23@gmail.com>
2026-06-21 12:10:47 +0000

    Update walletRoutes.js

--- src/routes/walletRoutes.js
+++ src/routes/walletRoutes.js
@@ -1,18 +1,17 @@
 import express from "express";
-
 import auth from "../middleware/auth.js";
-
 import {
-  getMyWallet,
+  getMyWallet, initializePayment, verifyPayment,
+  testTopUp, sendTip, getTransactions,
 } from "../controllers/walletController.js";
 
-const router =
-  express.Router();
+const router = express.Router();
 
-router.get(
-  "/",
-  auth,
-  getMyWallet
-);
+router.get("/", auth, getMyWallet);
+router.get("/transactions", auth, getTransactions);
+router.post("/topup", auth, initializePayment);
+router.post("/topup/test", auth, testTopUp);
+router.get("/verify/:reference", auth, verifyPayment);
+router.post("/tip", auth, sendTip);
 
 export default router;
