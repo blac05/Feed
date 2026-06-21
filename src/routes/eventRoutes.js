c4a0beb3ea06523fe58e6cb8ae030bd4f687d487
Joe Jae <blaqj23@gmail.com>
2026-06-21 03:23:18 +0000

    Update eventRoutes.js

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,27 +1,15 @@
 import express from "express";
-
 import auth from "../middleware/auth.js";
-
 import {
-  createEvent,
-  getEvents,
-  deleteEvent,
+  getEvents, getEvent, createEvent, rsvpEvent, deleteEvent
 } from "../controllers/eventController.js";
 
 const router = express.Router();
 
 router.get("/", getEvents);
-
-router.post(
-  "/",
-  auth,
-  createEvent
-);
-
-router.delete(
-  "/:id",
-  auth,
-  deleteEvent
-);
+router.get("/:id", getEvent);
+router.post("/", auth, createEvent);
+router.post("/:id/rsvp", auth, rsvpEvent);
+router.delete("/:id", auth, deleteEvent);
 
 export default router;
