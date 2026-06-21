a72a8c9ab31e978de7fd799ac660344e9cc40da4
Joe Jae <blaqj23@gmail.com>
2026-06-21 12:12:32 +0000

    Update productRoutes.js

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,23 +1,17 @@
 import express from "express";
 import auth from "../middleware/auth.js";
-
 import {
-  createProduct,
-  getProducts,
+  getProducts, getProduct, createProduct,
+  updateProduct, deleteProduct, likeProduct,
 } from "../controllers/productController.js";
 
-const router =
-  express.Router();
+const router = express.Router();
 
-router.post(
-  "/",
-  auth,
-  createProduct
-);
-
-router.get(
-  "/",
-  getProducts
-);
+router.get("/", getProducts);
+router.get("/:id", getProduct);
+router.post("/", auth, createProduct);
+router.put("/:id", auth, updateProduct);
+router.delete("/:id", auth, deleteProduct);
+router.put("/:id/like", auth, likeProduct);
 
 export default router;
