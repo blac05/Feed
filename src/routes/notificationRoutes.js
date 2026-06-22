eaa68f3e706f27888e277895b75b5a53f1c12f3a
Joe Jae <blaqj23@gmail.com>
2026-06-22 03:07:19 +0000

    Update notificationRoutes.js

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -1,23 +1,16 @@
 import express from "express";
 import auth from "../middleware/auth.js";
-
 import {
-  getNotifications,
-  markAsRead,
+  getNotifications, markAllRead, markOneRead,
+  deleteNotification, clearAll,
 } from "../controllers/notificationController.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  auth,
-  getNotifications
-);
-
-router.put(
-  "/:id/read",
-  auth,
-  markAsRead
-);
+router.get("/", auth, getNotifications);
+router.put("/read-all", auth, markAllRead);
+router.put("/:id/read", auth, markOneRead);
+router.delete("/clear-all", auth, clearAll);
+router.delete("/:id", auth, deleteNotification);
 
 export default router;
