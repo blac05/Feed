f911cdccdff60cf6c693604669bb33a8efae8fdd
Joe Jae <blaqj23@gmail.com>
2026-06-22 02:55:10 +0000

    Update Notification.js

--- src/models/Notification.js
+++ src/models/Notification.js
@@ -1,42 +1,20 @@
 import mongoose from "mongoose";
 
-const notificationSchema =
-  new mongoose.Schema(
-    {
-      recipient: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-      },
-
-      sender: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-
-      type: {
-        type: String,
-        enum: [
-          "like",
-          "comment",
-          "follow",
-          "event",
-        ],
-      },
-
-      message: String,
-
-      read: {
-        type: Boolean,
-        default: false,
-      },
+const notificationSchema = new mongoose.Schema(
+  {
+    recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    type: {
+      type: String,
+      enum: ["like", "comment", "follow", "mention", "system", "gift"],
+      required: true,
     },
-    {
-      timestamps: true,
-    }
-  );
-
-export default mongoose.model(
-  "Notification",
-  notificationSchema
+    text: { type: String, required: true },
+    postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
+    link: { type: String, default: "" },
+    read: { type: Boolean, default: false },
+  },
+  { timestamps: true }
 );
+
+export default mongoose.model("Notification", notificationSchema);
