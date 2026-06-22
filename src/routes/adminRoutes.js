1365fe40d045378eaac857c085f19d9f3025262d
Joe Jae <blaqj23@gmail.com>
2026-06-22 03:05:37 +0000

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,21 +1,23 @@
 import express from "express";
-
 import auth from "../middleware/auth.js";
 import adminAuth from "../middleware/adminAuth.js";
-
 import {
-  getUsers
-}
-from "../controllers/adminController.js";
+  getStats, getUsers, verifyUser, banUser, unbanUser,
+  makeAdmin, deleteUserPost, getRecentActivity,
+} from "../controllers/adminController.js";
 
-const router =
-express.Router();
+const router = express.Router();
 
-router.get(
-  "/users",
-  auth,
-  adminAuth,
-  getUsers
-);
+// All admin routes require auth + adminAuth
+router.use(auth, adminAuth);
+
+router.get("/stats", getStats);
+router.get("/users", getUsers);
+router.get("/activity", getRecentActivity);
+router.put("/users/:id/verify", verifyUser);
+router.put("/users/:id/ban", banUser);
+router.put("/users/:id/unban", unbanUser);
+router.put("/users/:id/make-admin", makeAdmin);
+router.delete("/posts/:postId", deleteUserPost);
 
 export default router;
