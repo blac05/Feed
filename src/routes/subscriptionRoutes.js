16d3e5ab1b665759a465fa46102d70f59541d5dc
Joe Jae <blaqj23@gmail.com>
2026-06-25 02:18:20 +0000

    Update subscriptionRoutes.js

--- src/routes/subscriptionRoutes.js
+++ src/routes/subscriptionRoutes.js
@@ -1,24 +1,18 @@
 import express from "express";
 import auth from "../middleware/auth.js";
-
 import {
-  createSubscription,
-  getSubscriptions,
+  getCreatorTiers, setupTiers, subscribe, cancelSubscription,
+  getMySubscriptions, getMySubscribers, checkSubscription,
 } from "../controllers/subscriptionController.js";
 
-const router =
-  express.Router();
+const router = express.Router();
 
-router.post(
-  "/",
-  auth,
-  createSubscription
-);
-
-router.get(
-  "/",
-  auth,
-  getSubscriptions
-);
+router.get("/my", auth, getMySubscriptions);
+router.get("/subscribers", auth, getMySubscribers);
+router.get("/check/:creatorId", auth, checkSubscription);
+router.get("/tiers/:creatorId", getCreatorTiers);
+router.post("/tiers", auth, setupTiers);
+router.post("/subscribe", auth, subscribe);
+router.delete("/cancel/:creatorId", auth, cancelSubscription);
 
 export default router;
